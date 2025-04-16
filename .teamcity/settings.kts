e0878427b6cbe7c440fe72a211037cd3ccad113b
dkrupkina <dkrupkina@TeamCity>
2025-04-16 09:50:24 +0000

    bulk pause/activate with comment:  (TeamCity change in '<Root project>' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,9 +1,14 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.buildSteps.NUnitStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.DotnetMsBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.DotnetVsTestStep
 import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetBuild
-import jetbrains.buildServer.configs.kotlin.buildSteps.nunit
-import jetbrains.buildServer.configs.kotlin.buildSteps.nunitConsole
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetMsBuild
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetRestore
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetTest
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetVsTest
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.projectFeatures.kubernetesExecutor
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
@@ -28,34 +33,50 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2024.03"
+version = "2025.03"
 
 project {
 
-    buildType(NUnitConsoleRunner1dll)
-    buildType(NUnitConsoleRunnerDeprecatedNUnitRunner1dll)
+    buildType(TestLinuxAgent)
+    buildType(TestLinuxAgentDocker)
+    buildType(VSTestLinuxAgent)
+    buildType(VSTestLinuxAgentDocker)
+
+    features {
+        kubernetesExecutor {
+            id = "PROJECT_EXT_104"
+            connectionId = "PROJECT_EXT_2"
+            profileName = "K8s Executor"
+            buildsLimit = "1"
+            containerParameters = "teamcity.agent.jvm.os.name=Linux"
+            templateName = "ubuntu-agent"
+        }
+    }
 }
 
-object NUnitConsoleRunner1dll : BuildType({
-    name = "✔️ NUnit Console Runner: 1 dll"
+object TestLinuxAgent : BuildType({
+    name = "test, Linux agent"
+    paused = true
 
     vcs {
         root(DslContext.settingsRoot)
     }
 
     steps {
-        dotnetBuild {
-            name = "Build Project"
-            id = "Build_Project"
+        dotnetRestore {
+            id = "dotnet_1"
             projects = "NUnitTests.sln"
-            param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
         }
-        nunitConsole {
-            name = "nUnit tests"
-            id = "nUnit_tests"
-            nunitPath = "%teamcity.tool.NUnit.Console.3.17.0%"
-            includeTests = """NUnitTests\bin\Debug\net8.0\NUnitTests.dll"""
-            param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
+        dotnetMsBuild {
+            id = "dotnet"
+            projects = "NUnitTests.sln"
+            version = DotnetMsBuildStep.MSBuildVersion.CrossPlatform
+        }
+        dotnetTest {
+            id = "dotnet_2"
+            projects = "NUnitTests/NUnitTests.csproj"
+            coverage = dotcover {
+            }
         }
     }
 
@@ -66,43 +87,47 @@ object NUnitConsoleRunner1dll : BuildType({
 
     failureConditions {
         testFailure = false
-        nonZeroExitCode = false
     }
 
     features {
         perfmon {
         }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
     }
 })
 
-object NUnitConsoleRunnerDeprecatedNUnitRunner1dll : BuildType({
-    name = "NUnit Console Runner + Deprecated NUnit Runner: 1 dll"
+object TestLinuxAgentDocker : BuildType({
+    name = "test, Linux agent + Docker"
+    paused = true
+
+    artifactRules = "**/* => sources.zip"
 
     vcs {
         root(DslContext.settingsRoot)
     }
 
     steps {
-        dotnetBuild {
-            name = "Build Project"
-            id = "Build_Project"
+        dotnetRestore {
+            id = "dotnet_1"
             projects = "NUnitTests.sln"
-            param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
+            param("teamcity.kubernetes.executor.container.image", "mcr.microsoft.com/dotnet/sdk:8.0")
         }
-        nunitConsole {
-            name = "nUnit tests"
-            id = "nUnit_tests"
-            nunitPath = "%teamcity.tool.NUnit.Console.3.17.0%"
-            includeTests = """NUnitTests\bin\Debug\net8.0\NUnitTests.dll"""
-            param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
+        dotnetBuild {
+            id = "dotnet"
+            projects = "NUnitTests.sln"
+            param("teamcity.kubernetes.executor.container.image", "mcr.microsoft.com/dotnet/sdk:8.0")
         }
-        nunit {
-            name = "NUnit: deprecated, dll"
-            id = "NUnit_deprecated_dll"
-            nunitVersion = NUnitStep.NUnitVersion.NUnit_2_6_4
-            runtimeVersion = NUnitStep.RuntimeVersion.v4_0
-            includeTests = """NUnitTests\bin\Debug\net8.0\NUnitTests.dll"""
-            runProcessPerAssembly = true
+        dotnetTest {
+            id = "dotnet_2"
+            projects = "NUnitTests/NUnitTests.csproj"
+            dockerImage = "mcr.microsoft.com/dotnet/sdk:8.0"
+        }
+        script {
+            id = "simpleRunner"
+            scriptContent = """echo "Love cats!!""""
         }
     }
 
@@ -113,11 +138,109 @@ object NUnitConsoleRunnerDeprecatedNUnitRunner1dll : BuildType({
 
     failureConditions {
         testFailure = false
-        nonZeroExitCode = false
     }
 
     features {
         perfmon {
         }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+})
+
+object VSTestLinuxAgent : BuildType({
+    name = "VSTest, Linux agent"
+    paused = true
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        dotnetRestore {
+            id = "dotnet_1"
+            projects = "NUnitTests.sln"
+        }
+        dotnetMsBuild {
+            id = "dotnet"
+            projects = "NUnitTests.sln"
+            version = DotnetMsBuildStep.MSBuildVersion.CrossPlatform
+        }
+        dotnetVsTest {
+            id = "dotnet_2"
+            assemblies = "%system.teamcity.build.checkoutDir%/NUnitTests/bin/Debug/net8.0/NUnitTests.dll"
+            version = DotnetVsTestStep.VSTestVersion.CrossPlatform
+            platform = DotnetVsTestStep.Platform.Auto
+            coverage = dotcover {
+            }
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    failureConditions {
+        testFailure = false
+    }
+
+    features {
+        perfmon {
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+})
+
+object VSTestLinuxAgentDocker : BuildType({
+    name = "VSTest, Linux agent + Docker"
+    paused = true
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        dotnetRestore {
+            id = "dotnet_1"
+            projects = "NUnitTests.sln"
+            param("teamcity.kubernetes.executor.container.image", "mcr.microsoft.com/dotnet/sdk:8.0")
+        }
+        dotnetBuild {
+            id = "dotnet"
+            projects = "NUnitTests.sln"
+            param("teamcity.kubernetes.executor.container.image", "mcr.microsoft.com/dotnet/sdk:8.0")
+        }
+        dotnetVsTest {
+            id = "dotnet_2"
+            assemblies = "%system.teamcity.build.checkoutDir%/NUnitTests/bin/Debug/net8.0/NUnitTests.dll"
+            version = DotnetVsTestStep.VSTestVersion.CrossPlatform
+            platform = DotnetVsTestStep.Platform.Auto
+            dockerImage = "mcr.microsoft.com/dotnet/sdk:8.0"
+            dockerImagePlatform = DotnetVsTestStep.ImagePlatform.Linux
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    failureConditions {
+        testFailure = false
+    }
+
+    features {
+        perfmon {
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
     }
 })
