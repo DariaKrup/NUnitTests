0c2fa5fae5e7ad7b5d938473dd4f3a226481a93e
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2026-04-08 15:35:28 +0200

    Add a test

--- NUnitTests/UnitTest1.cs
+++ NUnitTests/UnitTest1.cs
@@ -14,6 +14,12 @@ public class Tests
     {
         Assert.Pass();
     }
+
+    [Test]
+    public void TestNew()
+    {
+        Assert.Pass();
+    }
     
     [Test]
     public void SuccessfulTestWithAnImage()
